ad108936324e1ceec5b20e867688bbdcc317edd6
Aleeja137 <66279116+Aleeja137@users.noreply.github.com>
2021-11-22 14:34:22 +0100

    2209.2
    Todo HECHO!!!!
    Lo único que me tirra es si debería reducir el ttl de los procesos cada ciclo de reloj o tener un temporizador para que vaya más lento

--- Source/clock.c
+++ Source/clock.c
@@ -19,63 +19,43 @@ void* clock_function(void * argument){
             pthread_cond_wait(&cond,&mtx);
             //esperar que uno a uno se despierten los timers
         }
+        reducirTtl(NULL);
         done_count=0;
         pthread_cond_broadcast(&cnd_br);
         pthread_mutex_unlock(&mtx);
     }
-
-    /*done_count = 0;
-    while (1)
-    {
-        printf("Barrera 1");
-        pthread_mutex_lock(&mtx);
-        while (done_count<num_timers)
-        {
-            pthread_cond_wait(&cond,&mtx);
-            printf("Barrera 2");
-            //esperar que uno a uno se despierten los timers
-        }
-        printf("Barrera 3");
-        done_count=0;
-        process_node_t* link = lista_procesos.first;printf("Barrera ");
-        while (link->next!=NULL){
-            printf("Barrera 4");
-           //Recorro los CPUs, cores e hilos reduciendo el ttl
-            for (int i = 0; i < num_CPUs; i++)
-            {
-                printf("Barrera 5");
-                for (int j = 0; j < num_cores; j++)
-                {
-                    printf("Barrera 6");
-                    for (int k = 0; k < num_processes; k++)
-                    {
-                        printf("Barrera 7");
-                        if (machine.cpu_array[i].core_array[j].process_array[k].asigned_core_id!=-1)
-                        {
-                            printf("Barrera 8");
-                            machine.cpu_array[i].core_array[j].process_array[k].ttl+=-1;
-                            //Si el ttl es cero, es decir, el proceso muere, no hace falta eliminarlo, 
-                            //solo poner a -1 el int de asignación y el mismo scheduler ya lo tiene en cuenta como libre
-                            if (machine.cpu_array[i].core_array[j].process_array[k].ttl == 0)
-                            {
-                                printf("Barrera 9");
-                                machine.cpu_array[i].core_array[j].process_array[k].asigned_core_id = -1;
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                }
-                
-            }
-        }
-        printf("Barrera 10");
-        pthread_cond_broadcast(&cnd_br);
-        pthread_mutex_unlock(&mtx);
-    }*/
 }
-
+int internal_count = 0; //variable para que no reduzca el ttl cada ciclo de reloj
 void* reducirTtl(void* arg){
-
+    if(internal_count==1000000)
+    {
+        //Recorro los CPUs, cores e hilos reduciendo el ttl
+        for (int i = 0; i < num_CPUs; i++)
+        {
+            for (int j = 0; j < num_cores; j++)
+            {
+                for (int k = 0; k < num_processes; k++)
+                {
+                    if (machine.cpu_array[i].core_array[j].process_array[k].asigned_core_id!=-1)
+                    {
+                        machine.cpu_array[i].core_array[j].process_array[k].ttl+=-1;
+                        printf("Reducida la vida de %d a %d\n",
+                            machine.cpu_array[i].core_array[j].process_array[k].pid,
+                            machine.cpu_array[i].core_array[j].process_array[k].ttl);
+                        //Si el ttl es cero, es decir, el proceso muere, no hace falta eliminarlo, 
+                        //solo poner a -1 el int de asignación y el mismo scheduler ya lo tiene en cuenta como libre
+                        if (machine.cpu_array[i].core_array[j].process_array[k].ttl == 0)
+                        {
+                            printf("Eliminado proceso %d \n",machine.cpu_array[i].core_array[j].process_array[k].pid);
+                            machine.cpu_array[i].core_array[j].process_array[k].asigned_core_id = -1;
+                        }            
+                    }            
+                }        
+            }        
+        }
+        internal_count=0;
+    } else {
+        internal_count++;
+    }
+       
 }
